4f04a9ca5173e886d2344a7500babf35f386b885
aqib-9 <rylegendaqib@gmail.com>
2024-10-16 00:50:18 +0530

    used json in place of txt and add error exceptions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 def generate():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -35,21 +36,52 @@ def generate():
     pyperclip.copy(password)
 
 def save():
+    info = {
+
+         website_entry.get()  : {
+            "email":email_entry.get(),
+            'password':password_entry.get()
+            }   
+          }
 
     if website_entry.get() == '' or  password_entry.get() == '':
         messagebox.showerror(title='Error',message='Please fill out all fields') 
     else:
+            
+            try:
+                with open('data.json','r') as f:
+                     data = json.load(f)
+            except  FileNotFoundError:
+                with open('data.json','w') as f:
+                    json.dump(info,f,indent=4)
 
+            else:
 
-        is_ok=messagebox.askokcancel(title='website', message=f"These are the details you entered \n Email:{email_entry.get()} \n website: {website_entry.get()} \n password:{password_entry.get()}")
+                data.update(info)
 
+                with open('data.json','w') as f:
+                    json.dump(data,f,indent=4)
+            finally:
+                website_entry.delete(0,END)
+                password_entry.delete(0,END)
 
-        if is_ok:
-            with open('data.txt',W) as f:
-                f.write(f'{website_entry.get()} |  {email_entry.get()} | {password_entry.get()} \n')
-            website_entry.delete(0,END)
-            password_entry.delete(0,END)
-
+def find_pass():
+    if website_entry.get() == '':
+        messagebox.showerror(title='Error', message='Please fill Website name')
+    else:
+          try:
+            with open('data.json','r') as f:
+                data = json.load(f)
+          except FileNotFoundError:
+                messagebox.showerror(title='Error',message=f'No data for {website_entry.get()} is saved')
+          else:
+                if  website_entry.get() in data:    
+                    email=data[website_entry.get()]['email']
+                    password=data[website_entry.get()]['password']
+                    messagebox.showinfo(title=website_entry.get(),message=f'Email:{email}\nPassword:{password}')
+                else:
+                    messagebox.showerror(title='Error',message='No details found')
+                  
 
 
 
@@ -66,10 +98,12 @@ canvas.grid(row=0, column=0, columnspan=3, pady=20)
 
 
 label_text = Label(text="Website:")
-label_text.grid(row=1, column=0, padx=5, pady=5)
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, padx=5, pady=5)
+label_text.grid(row=1, column=0, padx=0, pady=5)
+website_entry = Entry(width=19)
+website_entry.grid(row=1, column=1,padx=0, pady=5)
 website_entry.focus()
+search_btn = Button(text='Search',command=find_pass,width=15)
+search_btn.grid(row=1, column=2, padx=0, pady=5)
 
 email_text = Label(text="Email/Username:")
 email_text.grid(row=2, column=0, padx=5, pady=5)
@@ -82,7 +116,7 @@ password_text.grid(row=3, column=0, padx=0, pady=5)
 password_entry = Entry(width=19)  
 password_entry.grid(row=3, column=1, padx=0, pady=5)
 
-generate_btn = Button(text='Generate Password',command=generate)
+generate_btn = Button(text='Generate Password',command=generate,width=15)
 generate_btn.grid(row=3, column=2, padx=0, pady=5)
 
 
